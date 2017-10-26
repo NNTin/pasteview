bd4a6c2d2251c889123d569c178445e4a4f847ae
NNTin <nguyen.ngoctindaniel@gmail.com>
2017-10-26 21:12:18 +0200

    Seperate html, js, css

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const regex = /(?:http(?:s)?:\/\/)?pastebin.com\/(?:raw\/)?([A-z0-9]*)/;
+
+$(document).ready(function() {
+  retrieve();
+});
+
+function retrieve() {
+  var url = document.getElementById("url").value;
+  if (isPastebinUrl(url)) {
+    var request = makeHttpObject();
+    newUrl = getPastebinID(url);
+
+    request.open("GET", newUrl, true);
+    request.send(null);
+    request.onreadystatechange = function() {
+
+      if (request.readyState == 4) {
+        if (request.status == 0) {
+          alert("CORS not enabled.");
+        } else if (request.status == 200) {
+          display_txt = request.responseText
+          document.getElementById("pastebinPaste").innerHTML = display_txt.replace(/\n/g, "<br />");
+        } else {
+          alert("Unhandled status");
+        }
+      }
+    };
+  }
+}
+
+function isPastebinUrl(url) {
+  if ((m = regex.exec(url)) !== null) return true;
+  else {
+    alert("This is not a pastebin URL!");
+    return false;
+  }
+}
+
+function getPastebinID(url) {
+  m = regex.exec(url);
+  return "https://pastebin.com/raw/" + m[1].toString();
+}
+
+function makeHttpObject() {
+  try {
+    return new XMLHttpRequest();
+  } catch (error) {}
+  try {
+    return new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (error) {}
+  try {
+    return new ActiveXObject("Microsoft.XMLHTTP");
+  } catch (error) {}
+  throw new Error("Could not create HTTP request object.");
+}
