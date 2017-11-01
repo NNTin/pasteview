28cbc20ab9e60909bfe86caef1f7f4736eeb608d
NNTin <nguyen.ngoctindaniel@gmail.com>
2017-11-01 18:46:04 +0100

    rearrange and deploy

--- src/components/top.vue
+++ src/components/top.vue
@@ -3,42 +3,50 @@
 		<a id="githubmark" href="https://github.com/nntin/pasteview">
 			<img src="../assets/githubmark.png">
 		</a>
-		<div v-show="pastebinURL">
-		<label>Displaying following paste:<a :href="pastebinURL">{{pastebinURL}}</a></label>
-		<label>Word count: {{wordCount}}</label>
-		<br/>
-		<br/>
+		<div>
+			<div v-show="pastebinURL">
+				<label>Displaying following paste:<a :href="pastebinURL">{{pastebinURL}}</a></label>
+				<label>Word count: {{wordCount}}</label>
+				<br/>
+				<br/>
+			</div>
+			<div id="sliders">
+				<div id="text-slider">
+					<label id="description">Text Slider</label>
+					<vue-slider :show="showSlider" :tooltip=false v-on:input="textFormatChange" :min="20" :max="75" width="620%" v-model="textFormat.textSize"></vue-slider>
+				</div>
+				<br/>
+				<div id="chrome-color">
+					<label id="description">Background Color</label>
+					<chrome-picker v-model="textFormat.colorsBackground"/>
+				</div>
+				<div id="chrome-color">
+					<label id="description">Text Color</label>
+					<chrome-picker v-model="textFormat.colorsText"/>
+				</div>
+			</div>
+			<div id="buttons">
+				<div>
+					<button title="Save slider and color settings" :style="getSaveButtonStyle" v-on:click="save">Save</button>
+					<button title="Reset settings to default" :style="getButtonStyle('#FFFFFF','#000000')" v-on:click="reset">Reset</button>
+					<br/>
+					<button title="Set color" :style="getButtonStyle('#FFFFFF','#A7660C')" v-on:click="setColor('#FFFFFF','#A7660C')">Set color</button>
+					<button title="Set color" :style="getButtonStyle('#FFFFFF','#A70C0C')" v-on:click="setColor('#FFFFFF','#A70C0C')">Set color</button>
+					<br/>
+					<button title="Set color" :style="getButtonStyle('#FFFFFF','#770CA7')" v-on:click="setColor('#FFFFFF','#770CA7')">Set color</button>
+					<button title="Set color" :style="getButtonStyle('#FF9393','#FFFFFF')" v-on:click="setColor('#FF9393','#FFFFFF')">Set color</button>
+					<br/>
+					<button title="Set color" :style="getButtonStyle('#C79347','#FFFFFF')" v-on:click="setColor('#C79347','#FFFFFF')">Set color</button>
+					<button title="Set color" :style="getButtonStyle('#4793C7','#FFFFFF')" v-on:click="setColor('#4793C7','#FFFFFF')">Set color</button>
+					<br/>
+					<button title="Set color" :style="getButtonStyle('#383838','#FFFFFF')" v-on:click="setColor('#383838','#FFFFFF')">Set color</button>
+					<button title="Set color" :style="getButtonStyle('#FFBABA','#000000')" v-on:click="setColor('#FFBABA','#000000')">Set color</button>
+					<br/>
+					<button title="Set color" :style="getButtonStyle('#BAFF71','#000000')" v-on:click="setColor('#BAFF71','#000000')">Set color</button>
+					<button title="Set color" :style="getButtonStyle('#83BDFF','#000000')" v-on:click="setColor('#83BDFF','#000000')">Set color</button>
+			</div>
 		</div>
-
-		<div id="chrome-color">
-			<label id="description">Background Color</label>
-			<chrome-picker v-model="textFormat.colorsBackground"/>
 		</div>
-		<div id="chrome-color">
-			<label id="description">Text Color</label>
-			<chrome-picker v-model="textFormat.colorsText"/>
-		</div>
-		<div id="text-slider">
-			<label id="description">Text Slider</label>
-			<vue-slider :show="showSlider" :tooltip=false v-on:input="textFormatChange" :min="25" :max="75" width="300%" v-model="textFormat.textSize"></vue-slider>
-		</div>
-		<div id="buttons">
-			<button title="Save slider and color settings" :style="getSaveButtonStyle" v-on:click="save">Save</button>
-			<button title="Reset settings to default" :style="getButtonStyle('#FFFFFF','#000000')" v-on:click="reset">Reset</button>
-			<button title="Set color" :style="getButtonStyle('#FFFFFF','#A7660C')" v-on:click="setColor('#FFFFFF','#A7660C')">Set color</button>
-			<button title="Set color" :style="getButtonStyle('#FFFFFF','#A70C0C')" v-on:click="setColor('#FFFFFF','#A70C0C')">Set color</button>
-			<button title="Set color" :style="getButtonStyle('#FFFFFF','#770CA7')" v-on:click="setColor('#FFFFFF','#770CA7')">Set color</button>
-
-			<button title="Set color" :style="getButtonStyle('#FF9393','#FFFFFF')" v-on:click="setColor('#FF9393','#FFFFFF')">Set color</button>
-			<button title="Set color" :style="getButtonStyle('#C79347','#FFFFFF')" v-on:click="setColor('#C79347','#FFFFFF')">Set color</button>
-			<button title="Set color" :style="getButtonStyle('#4793C7','#FFFFFF')" v-on:click="setColor('#4793C7','#FFFFFF')">Set color</button>
-			<button title="Set color" :style="getButtonStyle('#383838','#FFFFFF')" v-on:click="setColor('#383838','#FFFFFF')">Set color</button>
-
-			<button title="Set color" :style="getButtonStyle('#FFBABA','#000000')" v-on:click="setColor('#FFBABA','#000000')">Set color</button>
-			<button title="Set color" :style="getButtonStyle('#BAFF71','#000000')" v-on:click="setColor('#BAFF71','#000000')">Set color</button>
-			<button title="Set color" :style="getButtonStyle('#83BDFF','#000000')" v-on:click="setColor('#83BDFF','#000000')">Set color</button>
-		</div>
-
 
 		<div id="textArea">
 			<div id="column1">
@@ -175,8 +183,9 @@ export default {
 		},
 		getButtonStyle: function (backgroundColor, textColor) {
 			return {
+				width: "48%",
 			  color: textColor,
-			  fontSize: '15px',
+			  fontSize: '12px',
 			  background: backgroundColor,
 			  padding: '10px 20px 10px 20px',
 			  border: 'solid #000000 3px'
@@ -204,8 +213,9 @@ export default {
 		getSaveButtonStyle: {
 			get: function () {
 				return {
+					width: "48%",
 				  color: this.textFormat.colorsText.hex,
-				  fontSize: '15px',
+				  fontSize: '12px',
 				  background: this.textFormat.colorsBackground.hex,
 				  padding: '10px 20px 10px 20px',
 				  border: 'solid #000000 3px'
@@ -221,7 +231,7 @@ export default {
 		float:left;
 		width:33%;
 	}
-	div#chrome-color, div#text-slider, div#buttons{
+	div#chrome-color, div#text-slider, div#buttons, div#sliders{
 		display: inline-block;
 	}
 	a#githubmark{
