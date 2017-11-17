2ee7010c0a446e146710f05eadc2f2aba2354f6b
NNTin <nguyen.ngoctindaniel@gmail.com>
2017-11-17 02:56:53 +0100

    click area fix

--- src/components/top.vue
+++ src/components/top.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="top">
 		<div class="github-corner">
-		  <svg width="80" height="80" viewBox="0 0 250 250" :style="getOctocatStyle">
-					<a href="https://github.com/NNTin/pasteview" :fill="textFormat.colorsText.hex">
+		  <svg width="120" height="120" viewBox="0 0 250 250" :style="getOctocatStyle">
+					<a href="https://github.com/NNTin/pasteview" :fill="textFormat.colorsText.hex" style="pointer-events:auto">
 				    <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
 					</a>
 			    <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
@@ -240,6 +240,7 @@ export default {
 				return {
 					fill: this.textFormat.colorsText.hex,
 					color: this.textFormat.colorsBackground.hex,
+					pointerEvents: "none",
 					position: 'absolute',
 					top: 0,
 					border: 0,
